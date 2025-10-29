65a89c2fd3cf79beb3b565737607acf7819eeb18
unknown <galamouyal88@gmail.com>
2025-10-29 20:07:46 +0200

    a

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,14 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+
+
+
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideHttpClient(),
-        provideBrowserGlobalErrorListeners(),
-        provideZonelessChangeDetection(),
         provideRouter(routes)
-
     ],
 };
